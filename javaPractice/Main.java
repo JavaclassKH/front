6717ce41af1e90d4c63a69a2ccc43014e14385af
javaclassKH <kh980614@gmail.com>
2024-04-16 17:55:19 +0900

    20240416_1755

--- javaPractice/Main.java
+++ javaPractice/Main.java
@@ -1,6 +1,8 @@
 package javaPractice;
 public class Main {
   public static void main(String[] args) {
+   System.out.println("다친대도 길을 걸어 kiss me 쉽지 않음 내가 쉽게 easy");
+   System.out.println("stage위엔 불이 튀어 my body pull up and I rip it up like ballet");
     
   }
 }
--- javaPractice/test121212.java
+++ javaPractice/test121212.java
@@ -1,21 +1,28 @@
+package javaPractice;
 import java.util.Scanner;
 
 class master {
   public static void main(String[] args) {  
     Scanner sc = new Scanner(System.in);
     int x = 0, y = 0  , ans = 0;
+    boolean run = true;
 
-    while (true) {
-      
+    while (run) {
       System.out.print("곱하실 첫번째 수를 입력하세요 : ");
       x = sc.nextInt();
+      if(x == 999) run = false;
       System.out.print("곱하실 두번째 수를 입력하세요 : ");
       y = sc.nextInt();
       ans = x * y;
+
   
       System.out.println("두 수의 곱은 '" + ans + "' 입니다.");
-    }
 
+      for(int i=0; i<=9; i++) {
+        System.out.println(i+"번: " + i);
+      }
+    }
+    System.out.println("시스템 종료");
   }
 }
 
